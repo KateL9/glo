c5bfa43d826e0beff8330135bfa6e205a35605dd
Kate Kunnikova <levchenkokaite@gmail.com>
2020-12-15 01:22:18 +0200

    init: timer project

--- script.js
+++ script.js
@@ -1,160 +1,44 @@
-'use strict';
-let start = document.getElementById('start'),
-    incomePlus = document.querySelector('.income_add'),
-    expensesPlus = document.querySelector('.expenses_add'),
-    checkbox = document.querySelector('.deposit-checkmark'),
-    additionalIncomeItems = document.querySelectorAll('.additional_income-item'),
-    budgetMonthVal = document.querySelector('.budget_month-value'),
-    budgetDayVal = document.querySelector('.budget_day-value'),
-    expensesMonthVal = document.querySelector('.expenses_month-value'),
-    additionalIncomeValue = document.querySelector('.additional_income-value'),
-    additionalExpensesValue = document.querySelector('.additional_expenses-value'),
-    incomePeriodVal = document.querySelector('.income_period-value'),
-    targetMonth = document.querySelector('.target_month-value'),
-    salaryAmount = document.querySelector('.salary-amount'),
-    expensesItems = document.querySelectorAll('.expenses-items'),
-    additionalExpensesItem = document.querySelector('.additional_expenses-item'),
-    targetAmount = document.querySelector('.target-amount'),
-    periodSelect = document.querySelector('.period-select'),
-    periodAmount = document.querySelector('.period-amount'),
-    incomeItems = document.querySelectorAll('.income-items');
+window.addEventListener('DOMContentLoaded', () => {
 
-let isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n)
-};
 
-let appData = {
-    income: {},
-    incomeMonth: 0,
-    addIncome: [],
-    expenses: {},
-    addExpenses: [],
-    deposit: false,
-    percentDeposit: 0,
-    moneyDeposit: 0,
-    budget: 0,
-    budgetDay: 0,
-    budgetMonth: 0,
-    expensesMonth: 0,
-    start: function() {
-        appData.budget = +salaryAmount.value;
-        appData.getExpenses();
-        appData.getIncome();
-        appData.getExpensesMonth();
-        appData.getAddExpenses();
-        appData.getAddIncome();
-        appData.getBudget();
+    // Timer
+    function countTimer(deadline) {
+        let timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
 
-        appData.showResult();
-    },
-    showResult: function() {
-        budgetMonthVal.value = appData.budgetMonth;
-        budgetDayVal.value = appData.budgetDay;
-        expensesMonthVal.value = appData.expensesMonth;
-        additionalExpensesValue.value = appData.addExpenses.join(', ');
-        additionalIncomeValue.value = appData.addIncome.join(', ');
-        targetMonth.value = appData.getTargetMonth();
-        incomePeriodVal.value = appData.calcPeriod();
-        periodSelect.addEventListener('input', function() {
-            incomePeriodVal.value = appData.calcPeriod();
-        });
-
-    },
-    addExpensesBlock: function() {
-        let cloneExpensesItem = expensesItems[0].cloneNode(true);
-        cloneExpensesItem.childNodes[1].value = '';
-        cloneExpensesItem.childNodes[3].value = '';
-        expensesItems[0].parentNode.insertBefore(cloneExpensesItem, expensesPlus);
-        expensesItems = document.querySelectorAll('.expenses-items');
-        if (expensesItems.length == 3) {
-            expensesPlus.style.display = 'none';
+        function getTimeRemaining() {
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+            return { timeRemaining, hours, minutes, seconds };
         }
-    },
 
-    getExpenses: function() {
-        expensesItems.forEach(function(item) {
-            let itemExpenses = item.querySelector('.expenses-title').value;
-            let cashExpenses = item.querySelector('.expenses-amount').value;
-            if (itemExpenses !== '' && cashExpenses !== '') {
-                appData.expenses[itemExpenses] = Number(cashExpenses);
+        function updateClock() {
+            let timer = getTimeRemaining();
+            if (timer.hours.toString().length <= 1) {
+                timerHours.textContent = '0' + timer.hours;
+            } else { timerHours.textContent = timer.hours; }
+            if (timer.minutes.toString().length <= 1) {
+                timerMinutes.textContent = '0' + timer.minutes;
+            } else { timerMinutes.textContent = timer.minutes; }
+            if (timer.seconds.toString().length <= 1) {
+                timerSeconds.textContent = '0' + timer.seconds;
+            } else { timerSeconds.textContent = timer.seconds; }
+            if (timer.timeRemaining <= 0) {
+                clearInterval(idInterval);
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+
             }
-        });
-    },
-    addIncomeBlock: function() {
-        let cloneIncomeItem = incomeItems[0].cloneNode(true);
-        cloneIncomeItem.childNodes[1].value = '';
-        cloneIncomeItem.childNodes[3].value = '';
-        incomeItems[0].parentNode.insertBefore(cloneIncomeItem, incomePlus);
-        incomeItems = document.querySelectorAll('.income-items');
-        if (incomeItems.length == 3) {
-            incomePlus.style.display = 'none';
         }
-    },
-    getIncome: function() {
-        incomeItems.forEach(function(item) {
-            let itemIncome = document.querySelector('.income-title').value;
-            let cashIncome = item.querySelector('.income-amount').value;
-            if (itemIncome !== '' && cashIncome !== '') {
-                appData.income[itemIncome] = cashIncome;
-            }
-        })
-    },
-    getAddExpenses: function() {
-        let addExpenses = additionalExpensesItem.value.split(',');
-        addExpenses.forEach(function(item) {
-            item = item.trim();
-            if (item !== '') {
-                appData.addExpenses.push(item);
-            }
-        });
-    },
-    getAddIncome: function() {
-        additionalIncomeItems.forEach(function(item) {
-            let itemValue = item.value.trim();
-            if (itemValue !== '') {
-                appData.addIncome.push(itemValue);
-            }
-        })
-    },
-    getExpensesMonth: function() {
-        for (let key in appData.expenses) {
-            appData.expensesMonth += appData.expenses[key];
-        }
-    },
-    getBudget: function() {
-        appData.budgetMonth = +appData.budget + Number(appData.incomeMonth) - appData.expensesMonth;
-        appData.budgetDay = Math.floor(appData.budgetMonth / 30);
-    },
-    getTargetMonth: function() {
-        let res = Math.round(targetAmount.value / appData.budgetMonth);
-        if (!isFinite(res) || res < 0) {
-            alert('Неверный ввод!');
-            return res = 0;
-        } else {
-            return res;
-        }
-    },
-    changeRange: function() {
-        periodAmount.innerHTML = periodSelect.value;
-    },
-    calcPeriod: function() {
-        return appData.budgetMonth * periodSelect.value;
+
+        const idInterval = setInterval(updateClock, 10);
     }
-};
-start.addEventListener('click', appData.start);
 
-expensesPlus.addEventListener('click', appData.addExpensesBlock);
-incomePlus.addEventListener('click', appData.addIncomeBlock);
-periodSelect.addEventListener('input', appData.changeRange);
-
-let main = document.querySelector('.main');
-main.addEventListener('input', function(e) {
-    console.log(e.target.value, e.target.placeholder);
-    if (e.target.placeholder === "Наименование") {
-        console.log(e.target.placeholder);
-        e.target.value = e.target.value.replace(/[^а-яё\s,.!:;'-]/i, '');
-    } else if (e.target.placeholder === "Сумма") {
-        console.log(e.target.placeholder);
-        e.target.value = e.target.value.replace(/[^\d]/g, '');
-    }
+    countTimer('16 december 2020');
 });
